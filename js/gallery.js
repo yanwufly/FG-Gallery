3c15ea71f853b17995066992fc6b11523136e3dc
woody <woody180@rocketmail.com>
2019-04-10 13:35:09 +0400

    gallery engine update

--- js/gallery.js
+++ js/gallery.js
@@ -1,255 +1,143 @@
-///////////////////////////////////////////////////// GET IMAGE PROPORTIONS /////////////////////////////////////////////////////
-// Get proportion
-function getImageProportions(x) {
-    let getImgWidth, getImgHeight
-    let winWidth = window.innerWidth;
-    let winHeight = window.innerHeight;
-
-    // IMAGE WIDTH
-    let ImagePopWidth = x.tagName === 'IMG' ? x.naturalWidth : x.getAttribute('data-width');
-    // IMAGE HEIGHT
-    let ImagePopHeight = x.tagName === 'IMG' ? x.naturalHeight : x.getAttribute('data-height');
-    // IMAGE PROPORTION
-    let imageProportion = ImagePopHeight / ImagePopWidth;
-
-    ImagePopWidth = winWidth;
-    ImagePopHeight = ImagePopWidth * imageProportion;
-    getImgWidth = ImagePopWidth - 120;
-    getImgHeight = ImagePopHeight - 120 * imageProportion;
+let FgGallery = function(selector) {
+    isTrue = false;
+    this.argumens = arguments;
+    this.gallerySelector = document.querySelectorAll(selector);
+    this.activeImage; // active (open) image
+    this.bodyCover = document.createElement('div');
     
-    // START IF
-    if (ImagePopWidth > winWidth) { // WIDTH
-        ImagePopWidth = winWidth;
-        ImagePopHeight = ImagePopWidth * imageProportion;
-        igetImgWidth = ImagePopWidth - 120;
-        getImgHeight = ImagePopHeight - 120 * imageProportion;
-    } else if (ImagePopHeight > winHeight) { // HEIGHT
-        ImagePopHeight = winHeight;
-        ImagePopWidth = ImagePopHeight / imageProportion;
-        getImgHeight = ImagePopHeight - 90;
-        getImgWidth = ImagePopWidth - 90 / imageProportion;
+    if (!document.querySelector('.body-cover')) {
+        document.body.appendChild(this.bodyCover);
+        this.bodyCover.classList.add('body-cover');
+        document.body.appendChild(this.bodyCover);
+        var bodyCoverDeps = `<div>
+            <div class="close-btn">
+                <svg class="close-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.9 21.9" xmlns:xlink="http://www.w3.org/1999/xlink">   <path class="close-svg" d="M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z"/> </svg>
+            </div>
+            <div class="gallery-arrow next-btn"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 492.004 492.004" xml:space="preserve"> <g> <g> <path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12 c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028 c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265 c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"/> </g> </g> </svg></div>
+            <div class="gallery-arrow prev-btn"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 492 492" xml:space="preserve"> <g> <g> <path d="M198.608,246.104L382.664,62.04c5.068-5.056,7.856-11.816,7.856-19.024c0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12 C361.476,2.792,354.712,0,347.504,0s-13.964,2.792-19.028,7.864L109.328,227.008c-5.084,5.08-7.868,11.868-7.848,19.084 c-0.02,7.248,2.76,14.028,7.848,19.112l218.944,218.932c5.064,5.072,11.82,7.864,19.032,7.864c7.208,0,13.964-2.792,19.032-7.864 l16.124-16.12c10.492-10.492,10.492-27.572,0-38.06L198.608,246.104z"/> </g> </g> </svg></div>
+        </div>`;
+        bodyCoverDeps = document.createRange().createContextualFragment(bodyCoverDeps).firstElementChild
+        this.bodyCover.appendChild(bodyCoverDeps);
     }
 
-    let obj = {
-        width: getImgWidth,
-        height: getImgHeight
-    }
-    return obj;
+    this.nextBtn = document.querySelector('.next-btn'); // next button
+    this.prevBtn = document.querySelector('.prev-btn'); // previous button
+    this.closeBtb = document.querySelector('.close-btn'); // close image button
+
+    this.gallerySequence = 0; // gallery
+    this.galleryItemSequence = 0; // gallery image
+
+    // Adding cols class
+    this.gallerySelector[0].classList.add(`cols-${this.argumens[1].cols}`);
+
+    // Original images
+    this.originalImages = {} // galleries with images
+
+    // init functions
+    this.generateItems();
+    this.closeImage();
+    this.styles();
 }
 
-
-///////////////// GALLERY /////////////////
-let FgGallery = function() {
+// Generate image items
+FgGallery.prototype.generateItems = function() {
     let This = this;
-    // main container
-    this.fgContainer = document.querySelector(arguments[0]);
-    // gallery setup
-    this.setup = arguments[1]
-    this.setup && this.setup.cols ? this.fgContainer.classList.add(`cols-${this.setup.cols}`) : false;
-    // gallery images
-    this.items = this.fgContainer.querySelectorAll('img');
-    // items length
-    this.itemsLength = this.items.length;
+    this.gallerySelector.forEach(function(galleryContainer, i) {
+        This.originalImages[`gallery${i}`] = []
 
-    // create gallery data
-    this.galleryData = document.createElement('div');
-    this.galleryData.classList.add('gallery-data');
-    document.body.appendChild(this.galleryData);
+        galleryContainer.querySelectorAll('img').forEach(function(imageItems, key) {
+            This.originalImages[`gallery${i}`].push(imageItems); // Save original images
 
-    // current image index
-    this.current = 0;
-    
-    // create next, previous buttons & close
-    this.nextBtn = document.createElement('div');
-    this.nextBtn.classList.add('next-btn', 'gallery-arrow')
-    this.galleryData.appendChild(this.nextBtn);
-    this.nextBtn.innerHTML = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" 	 viewBox="0 0 492.004 492.004"  xml:space="preserve"> <g> 	<g> 		<path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12 			c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028 			c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265 			c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"/> 	</g> </g> </svg>'
-    
-    this.prevBtn = document.createElement('div');
-    this.prevBtn.classList.add('prev-btn', 'gallery-arrow')
-    this.galleryData.appendChild(this.prevBtn);
-    this.prevBtn.innerHTML = '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" 	viewBox="0 0 451.847 451.847" 	 xml:space="preserve"> <g> 	<path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0 		c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744 		c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"/> </g> </svg>'
-    
-    this.closeBtn = document.createElement('div');
-    this.closeBtn.classList.add('close-btn');
-    this.closeBtn.innerHTML = '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" 	viewBox="0 0 348.333 348.334"  xml:space="preserve"> <g> 	<path d="M336.559,68.611L231.016,174.165l105.543,105.549c15.699,15.705,15.699,41.145,0,56.85 		c-7.844,7.844-18.128,11.769-28.407,11.769c-10.296,0-20.581-3.919-28.419-11.769L174.167,231.003L68.609,336.563 		c-7.843,7.844-18.128,11.769-28.416,11.769c-10.285,0-20.563-3.919-28.413-11.769c-15.699-15.698-15.699-41.139,0-56.85 		l105.54-105.549L11.774,68.611c-15.699-15.699-15.699-41.145,0-56.844c15.696-15.687,41.127-15.687,56.829,0l105.563,105.554 		L279.721,11.767c15.705-15.687,41.139-15.687,56.832,0C352.258,27.466,352.258,52.912,336.559,68.611z"/> </g> </svg>';
-    this.galleryData.insertBefore(this.closeBtn, this.galleryData.childNodes[0]);
-    
-    // create function
-    this.createFunc();
+            // create new image elements
+            var newImages = '';
+            newImages += `<div class="gallery-items" style="background: url(${imageItems.src}) center / cover; "></div>`
+            newImages = galleryContainer.appendChild(document.createRange().createContextualFragment(newImages).firstElementChild);
+            imageItems.remove(); // Remove original images
+            
+            // open image
+            newImages.onclick = function() {
+                This.galleryItemSequence = key;
+                This.gallerySequence = i;
+                This.next();
+                This.back();
 
-    // next image
-    this.nextBtn.addEventListener('click', function() {
-        This.galNextFunc();
-    });
-
-    // previous image
-    this.prevBtn.addEventListener('click', function() {
-        This.galPrevFunc();
-    });
-
-    // close image //// close functions
-    this.closeBtn.addEventListener('click', function() {
-        This.galleryData.classList.remove('active');
-        This.galleryData.querySelectorAll('.data-items').forEach((openItems) => {
-            openItems.classList.remove('active');
-        });
-    });
-    this.galleryCloseFunc(function() {
-        This.galleryData.classList.remove('active');
-        This.galleryData.querySelectorAll('.data-items').forEach((openItems) => {
-            openItems.classList.remove('active');
-        });
-    });
-    document.addEventListener('keydown', function(e) {
-        if (e.keyCode === 39) {
-            This.galNextFunc();
-        }
-        if (e.keyCode === 37) {
-            This.galPrevFunc();
-        }
-        if (e.keyCode === 27) {
-            This.galleryData.classList.remove('active');
-            This.galleryData.querySelectorAll('.data-items').forEach((openItems) => {
-                openItems.classList.remove('active');
-            });
-        }
-    })
-}
-
-
-
-
-// dependences creation
-FgGallery.prototype.createFunc = function() {
-    let This = this;
-    let isTure = this.items[0].nextElementSibling !== null || undefined ? false : true;
-    this.items.forEach(function(items, i) {
-        // create containers for images
-        let divItems = isTure ? items.parentElement : document.createElement('div');
-        // add manual height
-        This.setup && This.setup.height ? divItems.style.height = `${This.setup.height}px` : false;
-
-        // styles setup
-        if (This.setup && This.setup.style) {
-            for (const key in This.setup.style) {
-                divItems.style[key] = "" + This.setup.style[key];
+                This.bodyCover.classList.add('active'); // active background cover
+                This.activeImage = This.originalImages[`gallery${i}`][key].cloneNode(true);
+                This.bodyCover.appendChild(This.activeImage);
+                imagePopResize(This.activeImage);
             }
-        }
-       
-        
-                    // console.log('items:',items);
 
-        // add class
-        divItems.classList.add('gallery-items')
-        // append items
-        divItems.appendChild(items);
-        This.fgContainer.appendChild(divItems);
-        // create gallery data items
-        let dataItems = document.createElement('div')
-        dataItems.classList.add('data-items');
-        This.galleryData.appendChild(dataItems);
-        
-        let inter = setInterval(function() {
-            if (getImageProportions(items).width && getImageProportions(items).height) {
-                clearInterval(inter)
-                dataItems.innerHTML += `<div data-sequence="${i}" style="width:${getImageProportions(items).width + 20}px; height:${getImageProportions(items).height + 20}px;" class="item-border"><img style="width: ${getImageProportions(items).width}px; height: ${getImageProportions(items).height}px;" src="${items.src}" /></div>`;
-
-                // change dimensions on resize
-                window.onresize = function() {
-                    for (let ii = 0; ii < This.itemsLength; ii++) {
-                        let imgCont = document.querySelectorAll('.item-border')[ii];
-                       
-                        imgCont.style.height = getImageProportions(This.items[ii]).height + 20 + 'px';
-                        imgCont.querySelector('img').style.height = getImageProportions(This.items[ii]).height + 'px';
-                        
-                        imgCont.style.width = getImageProportions(This.items[ii]).width + 20 + 'px';
-                        imgCont.querySelector('img').style.width = getImageProportions(This.items[ii]).width + 'px';
-                     }
+            // sizing image on window resize
+            window.addEventListener('resize', function() {
+				if (This.activeImage) {
+                    imagePopResize(This.activeImage);
                 }
-             
-            }
-        }, 10);
-        
-        let interval = setInterval(function() {
-            // create image data with image sizes
-            if (document.querySelectorAll('.item-border img')[i]) {
-                clearInterval(interval);
-                divItems.style.background = `url('${items.src}') no-repeat center center / cover`;
-                items.parentElement.removeChild(items);
-            }
-        }, 10);
+			})
+                
+            // sizing image
+            function imagePopResize(elem) {
+				if (elem.naturalWidth < window.innerWidth && elem.naturalHeight < window.innerHeight ) {
+					elem.style.width = 'auto';
+					elem.style.height = 'auto';
+				} 
 
-        // event listener
-        divItems.addEventListener('click', function(e) {
-            e.preventDefault();
-            This.current = i;
-            This.galSlide(i);
+				if (elem.offsetWidth > window.innerWidth) {
+                    elem.style.width = '80%';
+					elem.style.height = 'auto';
+				}
+
+				if (elem.offsetHeight > window.innerHeight) {
+                    elem.style.width = 'auto';
+					elem.style.height = '80%';
+				}
+			}
         })
     })
 }
 
-// NEXT FUNCTION
-FgGallery.prototype.galNextFunc = function() {
-    (this.current === this.itemsLength - 1) ? this.current = 0 : this.current +=1;
-    this.galSlide(this.current);
-}
-
-// PREVIOUS FUNCTION
-FgGallery.prototype.galPrevFunc = function() {
-    (this.current === 0 ) ? this.current = this.itemsLength - 1 : this.current -= 1;
-    this.galSlide(this.current);
-}
-
-// SELECT IMAGE & NONE OTHERS
-FgGallery.prototype.galSlide = function(index) {
-    let This = this;
-    if (index >= 0 && index <= this.itemsLength) { 
-        for (let i = 0; i < this.itemsLength; i++) {
-            if (i === index) {
-                This.galleryData.classList.add('active');
-                This.galleryData.querySelectorAll('.data-items')[i].classList.add('active');
-            } else {
-                This.galleryData.querySelectorAll('.data-items')[i].classList.remove('active');
-            }
+// next
+FgGallery.prototype.next = function() {
+    this.nextBtn.onclick = () => {
+        this.galleryItemSequence++
+        if (this.galleryItemSequence < this.originalImages[`gallery${this.gallerySequence}`].length) {
+            this.activeImage.src = this.originalImages[`gallery${this.gallerySequence}`][this.galleryItemSequence].src;
+        } else {
+            this.galleryItemSequence = 0
+            this.activeImage.src = this.originalImages[`gallery${this.gallerySequence}`][0].src;
         }
     }
 }
 
-// GALLERY CLOSE FUNCTION
-FgGallery.prototype.galleryCloseFunc = function(callback) {
-    let nm = 0;
-    document.addEventListener('click', function(ev) {
-        nm++;
-        if (ev.target.className === 'data-items' || ev.target.className === 'data-items active' && nm > 1) {
-            callback();
-            nm = 0;
+// back
+FgGallery.prototype.back = function() {
+    this.prevBtn.onclick = () => {
+        this.galleryItemSequence--;
+        if (this.galleryItemSequence > -1) {
+            this.activeImage.src = this.originalImages[`gallery${this.gallerySequence}`][this.galleryItemSequence].src
+        } else {
+            this.galleryItemSequence = this.originalImages[`gallery${this.gallerySequence}`].length - 1
+            this.activeImage.src = this.originalImages[`gallery${this.gallerySequence}`][this.galleryItemSequence].src
         }
-    });
+    }
 }
 
-
-
-
-
-var david = (function($){
-
-    $(document).ready(function(){
-    var view = (function(){
-        return {
-            galItems: $('.gallery-items'),
+// close image
+FgGallery.prototype.closeImage = function() {
+    this.bodyCover.onclick = (e) => {
+    if (e.target.classList.contains('active') || e.target.classList.contains('close-btn') || e.target.classList.contains('close-svg')) {
+            this.bodyCover.classList.remove('active');
+            this.bodyCover.querySelector('img').remove();
         }
-    })()
-
-    var funcs = (function(v){
-        console.log(v.galItems);
-    })(view)
-
-    return {
-        view:view,
-        funcs:funcs
     }
-})
+}
 
-
-})(jQuery)
+// applying styles
+FgGallery.prototype.styles = function() {
+    if (typeof this.argumens[1].style) {
+        for (const key in this.argumens[1].style) {
+            document.querySelectorAll(`${this.argumens[0]} .gallery-items`).forEach(items => {
+                items.style[key] = this.argumens[1].style[key]
+            })
+        }        
+    }
+}
